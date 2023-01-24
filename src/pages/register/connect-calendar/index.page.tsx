ef25419c08981a339bef1b8f6c1b93b2ea801611
João Paulo Mesquita <jp-mesquita@live.com>
2023-01-24 12:20:03 -0300

    feat: add message to authorization faill

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,15 +1,23 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { useSession, signIn, signOut } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
-import { ArrowRight } from 'phosphor-react'
+import { ArrowRight, Check } from 'phosphor-react'
 
 import { Container, Header } from '../styles'
-import { ConnectBox, ConnectItem } from './styles'
+import { AuthError, ConnectBox, ConnectItem } from './styles'
 
 export default function Register() {
   // async function handleRegister() {}
 
   const { data: session } = useSession()
+  const router = useRouter()
+
+  const hasAuthError = !!router.query.error
+
+  async function handleConnectCalendar() {
+    await signIn('google')
+  }
 
   return (
     <Container>
@@ -27,14 +35,15 @@ export default function Register() {
         <ConnectItem>
           <Text>Google Calendar</Text>
           {session ? (
-            <Button variant="secondary" size="sm" onClick={() => signOut()}>
-              Conectado, clique para sair
+            <Button disabled size="sm" onClick={() => signOut()}>
+              Conectado
+              <Check />
             </Button>
           ) : (
             <Button
               variant="secondary"
               size="sm"
-              onClick={() => signIn('google')}
+              onClick={handleConnectCalendar}
             >
               Conectar
               <ArrowRight />
@@ -42,7 +51,14 @@ export default function Register() {
           )}
         </ConnectItem>
 
-        <Button type="submit">
+        {hasAuthError && (
+          <AuthError size="sm">
+            Falaha ao se conectar ao Google, verifique se você habilitou as
+            permissões de acesso ao Google Calendar.
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={!session}>
           Próximo passo
           <ArrowRight />
         </Button>
